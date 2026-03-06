431abf007cfb82719230595ca325488514e0805d
Emerson de Oliveira Santos <106407466+EMERSONOL@users.noreply.github.com>
2026-03-06 15:56:56 -0300

    Update App Mapa de Calor.py

--- App Mapa de Calor.py
+++ App Mapa de Calor.py
@@ -39,7 +39,7 @@ def carregar_dados_parquet(caminho):
 # BARRA LATERAL
 st.sidebar.title("📁 Gestão de Dados")
 
-caminho_local = "anco de Dados.parquet"
+caminho_local = "Banco de Dados.parquet"
 gdf = None
 
 if os.path.exists(caminho_local):
@@ -159,4 +159,5 @@ if gdf is not None:
 else:
 
     st.info("👋 Verifique se o arquivo 'Banco de Dados.parquet' está na mesma pasta do script.")
+
 
